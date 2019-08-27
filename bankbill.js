94afed81d3fe35d7a5a48f5808a508d3c4fa622e
Evan <evanmlongwood@gmail.com>
2019-08-27 15:23:51 -0700

    Added new stuff.

--- bankbill.js
+++ bankbill.js
@@ -1,4 +1,4 @@
-const { prefix, token } = require('./config.json');
+const {prefix, token} = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -10,10 +10,10 @@ client.on('message', msg => {
   if (msg.content === `${prefix}ping`) {
     msg.reply('pong');
   } else if (msg.content === `${prefix}bread`) {
-      msg.reply('M  O  N  E  Y.');
+    msg.reply('M  O  N  E  Y.');
+  } else if (msg.content === `${prefix}server`) {
+    msg.channel.send(`This server's name is: ${msg.guild.name}`);
   }
 });
-
-
 
 client.login(token);
